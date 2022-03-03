d378410f24239fafebc97478f615fe690a137e77
Oliver Fu <ofu997@gmail.com>
2022-03-03 01:11:04 -0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { PortfolioProvider } from '../context/context';
-import { aboutData, contactData, footerData, heroData, projectsData } from '../mock/data';
+import { aboutData, contactData, footerData, heroData } from '../mock/data';
 import About from './About/About';
 import Contact from './Contact/Contact';
 import Hero from './Hero/Hero';
-// import Twitter from './Twitter/Twitter';
 
 function App() {
   const [hero, setHero] = useState({});
   const [about, setAbout] = useState({});
-  const [projects, setProjects] = useState([]);
   const [contact, setContact] = useState({});
   const [footer, setFooter] = useState({});
 
   useEffect(() => {
     setHero({ ...heroData });
     setAbout({ ...aboutData });
-    setProjects([...projectsData]);
     setContact({ ...contactData });
     setFooter({ ...footerData });
   }, []);
 
   return (
-    <PortfolioProvider value={{ hero, about, projects, contact, footer }}>
+    <PortfolioProvider value={{ hero, about, contact, footer }}>
       <Hero />
       <About />
-      {/* <Twitter /> */}
       <Contact />
     </PortfolioProvider>
   );

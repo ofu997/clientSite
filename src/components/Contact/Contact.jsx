2fffebd241b329f573f5377243129251b940e8cb
oliver fu <ofu997@gmail.com>
2023-12-12 21:26:34 -0800

    commit 12-12

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,15 +3,17 @@ import { Container } from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
 import { Link } from 'react-scroll';
 import Title from '../Title/Title';
-import {academia, email} from '../../../src/links';
+import { academia, email } from '../../links';
+import academiaImg from '../../images/academia.svg';
+import x from '../../images/x.svg';
 
 const Contact = () => {
   const networksArray = [
     {
       id: 1,
-      name: 'brands fa-x-twitter',
+      name: x,
       url: 'https://twitter.com/nellstra',
-    }, 
+    },
     {
       id: 2,
       name: 'linkedin',
@@ -19,19 +21,18 @@ const Contact = () => {
     },
     {
       id: 3,
-      name: 'book',
-      url: academia
+      name: academiaImg,
+      url: academia,
     },
-  ]
+  ];
 
   return (
     <section id="contact">
       <Container>
         <Title title="Contact" />
-        <div style={{ height: 40 }}></div>
+        <div style={{ height: 100 }} />
         <Fade bottom duration={1000} delay={800} distance="30px">
           <div className="contact-wrapper">
-
             <a
               target="_blank"
               rel="noopener noreferrer"
@@ -43,30 +44,29 @@ const Contact = () => {
           </div>
         </Fade>
 
-        <div style={{ height: 40 }}></div>
+        <div style={{ height: 100 }} />
         <div className="social-links">
-          {
-            networksArray.map((network) => {
-              const { id, name, url } = network;
-              return (
-                <a
-                  key={id}
-                  href={url}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                  aria-label={name}
-                >
-                  <i className={`fa fa-${name || 'refresh'} fa-inverse`} />
-                  <i className="fa fa-brand fa-x-twitter" />
-                </a>
-              );
-            })}
+          {networksArray.map((network) => {
+            const { id, name, url } = network;
+            return (
+              <a key={id} href={url} rel="noopener noreferrer" target="_blank" aria-label={name}>
+                {id === 2 ? (
+                  <div
+                    className={`fa fa-${name || 'refresh'} fa-inverse`}
+                    style={{ fontSize: '40px' }}
+                  />
+                ) : (
+                  <img src={name} height="30" width="30" alt="linkedin icon" />
+                )}
+              </a>
+            );
+          })}
         </div>
         <span className="back-to-top">
           <Link to="hero" smooth duration={1000}>
             <i className="fa fa-angle-up fa-2x" aria-hidden="true" />
           </Link>
-        </span>        
+        </span>
         <hr />
       </Container>
     </section>
